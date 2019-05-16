c6001b3b118a7f4019dd9bc8d2cbbed601dbc5cb
kindlich <kindlich@gmx.net>
2019-05-16 08:23:07 +0200

    Renamed Servlet in annotation

--- src/studigochi/test/Servlet.java
+++ src/studigochi/test/Servlet.java
@@ -7,11 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Abcd", value = "/main")
+@WebServlet(name = "Main", value = "/main")
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/Hidden.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
     }
 }
 

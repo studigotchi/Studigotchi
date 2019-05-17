68e6433a1371b607ceb25393e9bdc618e695c8ea
kindlich <kindlich@gmx.net>
2019-05-17 10:21:35 +0200

    Added UserModel.java

--- src/studigochi/test/Servlet.java
+++ src/studigochi/test/Servlet.java
@@ -1,17 +1,32 @@
 package studigochi.test;
 
+import studigochi.test.models.UserModel;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @WebServlet(name = "Main", value = "/main")
 public class Servlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/Hidden.jsp").forward(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            final Connection connection = DBServletContextListener.getConnection();
+            final PreparedStatement getOneUser = connection.prepareStatement("SELECT * from Users Where UserID=?");
+
+            getOneUser.setInt(1, 2);
+            getOneUser.execute();
+            resp.getWriter().println(new UserModel(getOneUser.getResultSet()));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
